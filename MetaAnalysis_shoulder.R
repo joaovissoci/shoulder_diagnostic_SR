7c986eb7b2f0f2864d12d83434c90dcd5de3e608
joaovissoci <joaovissoci@gmail.com>
2014-04-08 16:52:32 -0300

    Changing codes to update last version with minimum changes and updating qualitydata dataset with numerical factors

--- MetaAnalysis_shoulder.R
+++ MetaAnalysis_shoulder.R
@@ -19,7 +19,8 @@
 #install.packages("meta", repos="http://cran.r-project.org")
 
 #Loading packages
-lapply(c("metafor","ggplot2","car" ,"mada","RCurl","gdata","meta"), library, character.only=T)
+lapply(c("metafor","ggplot2","car" ,"mada","RCurl","gdata","meta",
+		"foreign"), library, character.only=T)
 
 #Load packages (after installed) with the library function
 #lapply(c("metafor","ggplot2","gridExtra" ,"psych", "RCurl", "irr","pgirmess", "nortest", "moments",
@@ -29,8 +30,7 @@ lapply(c("metafor","ggplot2","car" ,"mada","RCurl","gdata","meta"), library, cha
 ######################################################################################################
 #Data for shoulder subscapular diagnostic   
 #Importing data set from the Spredsheet in google docs (Insert link)
-webdata <- getURL("https://docs.google.com/spreadsheet/pub?key=0AgQJ5wHo6dDwdERNX2c2RGVtWjBiQzd0NW5teEZoMWc&single=true&gid=9&output=csv"
-                  ,ssl.verifypeer = FALSE)
+webdata <- getURL("https://raw.githubusercontent.com/joaovissoci/shoulder_diagnostic_SR/master/shoulder_diagnostic_SR_metadata.csv")
 data<-read.csv(textConnection(webdata)) #Readin .xml data into .csv
 #Recoding MRI subgroups categories
 data$MRI_Arthro<-car::recode(data$MRI_Arthro,"'arthro'='Arthro';'MRI'='MRI';else='Not Specified'")
@@ -49,20 +49,20 @@ specTesla<-with(data,data.frame(names,Positive,TP,Tesla))
 sensTesla<-with(data,data.frame(names,Negative,TN,Tesla))
 
 #Data for subscapular diagnostic SRMA - QUADAS 
-webqualitydata <- getURL("https://docs.google.com/spreadsheet/pub?key=0AgQJ5wHo6dDwdERNX2c2RGVtWjBiQzd0NW5teEZoMWc&single=true&gid=16&output=csv",ssl.verifypeer = FALSE)
+webqualitydata <- getURL("https://raw.githubusercontent.com/joaovissoci/shoulder_diagnostic_SR/master/shoulder_diagnostic_SR_qualitydata.csv",ssl.verifypeer = FALSE)
 qualitydata<-read.csv(textConnection(webqualitydata)) 
 
 ############################################################################
 #Figure 1. Quality Assessment
 ############################################################################
 summary(qualitydata)
-qualitydata$Item<-as.factor(qualitydata$Item)
 #attach(qualitydata)
 #Generate ggplot graph fro quality data information
 ggplot(qualitydata, aes(Item, Author)) + geom_tile(aes(fill = Value),
 colour = "white") + scale_fill_gradient(low = "white",
 high = "steelblue", name="", breaks=c(0,5,10), labels=c("No","Not Clear","Yes")) +
- theme(axis.text.x = element_text(hjust = 0, colour = "black",size=14),
+ theme(axis.text.x = element_text(angle 
+ = 330, hjust = 0, colour = "black",size=14),
         axis.text.y = element_text(colour = "black",size=14),
         axis.title.x = element_text(face="bold",size=14),
         axis.title.y = element_text(face="bold", size=14))
@@ -164,3 +164,36 @@ legend ("bottomright", c("Raw Data","Summary Estimate"),pch = c(2,1))
 #######################################################################################
 #should_diagnostic_script.R is licensed under a Creative Commons Attribution - Non commercial 3.0 Unported License. You are free: to Share — to copy, distribute and transmit the work to Remix — to adapt the work, under the following conditions: Attribution — You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work). Noncommercial — You may not use this work for commercial purposes. With the understanding that: Waiver — Any of the above conditions can be waived if you get permission from the copyright holder. Public Domain — Where the work or any of its elements is in the public domain under applicable law, that status is in no way affected by the license. Other Rights — In no way are any of the following rights affected by the license: Your fair dealing or fair use rights, or other applicable copyright exceptions and limitations; The author's moral rights; Rights other persons may have either in the work itself or in how the work is used, such as publicity or privacy rights. Notice — For any reuse or distribution, you must make clear to others the license terms of this work. The best way to do this is with a link to this web page. For more details see http://creativecommons.org/licenses/by-nc/3.0/
 #######################################################################################
+
+##TALITHA, VAMOS USAR ISSO?!?!?!! ############################
+#ROCellipse (primeiros 3 comandos nao estao rodando)
+metanalise2<-remove.vars(metanalise,"names")
+rs <- rowSums(metanalise2)
+weights <- 4 * rs / max(rs)
+crosshair(metanalise2, xlim = c(0,0.6), ylim = c(0.4,1),
+          col = 1:11, lwd = weights)
+ROCellipse(fit$ROCellipse, phc = "")
+points(spec(metanalise), sens(metanalise))
+#diagnostic odds ratio (DOR)
+(fit.DOR.DSL<-madauni(metanalise)) ## RANDOM EFFECT DIAG METANALYSIS
+summary(fit.DOR.DSL)
+(fit.DOR.MH<-madauni(metanalise, method = "MH"))
+summary(fit.DOR.MH)
+mada::forest(fit.DOR.MH)
+#proportional hazards model approach
+(fit.phm.homo <- phm(metanalise, hetero =  FALSE))
+(fit.phm.het <- phm(metanalise))
+#summary(fit.phm.homo)
+summary(fit.phm.het)
+#SROC curve
+plot(fit.phm.het, xlim = c(0,0.6), ylim = c(0.4,1))
+ROCellipse(metanalise, add = TRUE)
+#bivariate approach
+(fit.reitsma <- reitsma(metanalise))
+summary(fit.reitsma)
+plot(fit.reitsma, sroclwd = 2,
+    main = "SROC curve (bivariate model) for metanalise data")
+points(fpr(metanalise), sens(metanalise), pch = 2)
+legend ("bottomright", c("data","summary estimate"),pch = c(2,1))
+legend ("bottomleft", c("SROC", "conf.region"),lwd = c(2,1))
+
